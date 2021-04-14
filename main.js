cf0273af867275d2aacd1ea69dcfaf0843907275
VY12311 <79634026+VY12311@users.noreply.github.com>
2021-04-14 19:08:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log('[Info  - 6:34:40 PM] ESLint server is starting');
+console.log("[Info  - 6:34:41 PM] ESLint server running in node v12.18.3");
+console.log("[Info  - 6:34:41 PM] ESLint server is running.");
